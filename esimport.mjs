e7db753ae127e210594428fd5998467486411123
Johannes Maron <johannes@maron.family>
2025-04-24 01:01:49 +0200

    Finalize inital draft

--- esimport.mjs
+++ esimport.mjs
@@ -1,99 +1,223 @@
 #!/usr/bin/env node
 /**
- * Generate
+ * Compile a project into static JavaScript modules and generate a browser importmap.
+ *
+ * Usage:
+ *     esimport <package-root> <output-dir>
  */
 import * as esbuild from 'esbuild'
-import { argv, cwd } from 'node:process'
-import path from 'node:path';
+import * as process from 'node:process'
+import path from 'node:path'
 import fs from 'node:fs/promises'
-import { dependencies, peerDependencies, exports, imports, name, version} from 'package.json' assert { type: 'json' }
+import { fileURLToPath } from 'node:url'
+import { glob } from 'glob'
 
-const projectRoot = cwd()
-
-const options = {
-  bundle: true,
-  format: 'esm',
-  outbase: path.join(projectRoot, 'node_modules'),
-  outdir: argv[2],
-  external: Object.keys({ ...dependencies, ...peerDependencies, ...imports }),
-  minify: true,
-  sourcemap: true,
-  platform: 'browser',
-  target: 'es2020',
-  metafile: true,
+export async function integrityHash(data, algorithm = 'SHA-512') {
+  const ec = new TextEncoder()
+  const buffer = await globalThis.crypto.subtle.digest(algorithm, ec.encode(data))
+  const base64Hash = globalThis.btoa(String.fromCharCode(...new Uint8Array(buffer)))
+  return `${algorithm.toLowerCase().replace(/-/, '')}-${base64Hash}`
 }
-
-const banner = `/* Django-ESM - ${name}@${version} */`
-
-const entryPointMap = {}
 
 /**
- * Resolve a import path from a filesystem path and a subpath pattern.
+ * Recursively resolve the import paths from a package's entry point.
  *
- * @param {string} path - The relative filesystem path.
- * @param {string} entryPointPattern - The pattern to match the entry point.
- * @param {string} importPattern - The pattern to resolve the import path.
+ * See also: https://nodejs.org/api/packages.html#package-entry-points
+ *
+ * @param entryPoint {object|string|string[]}: The entry points (import|require|default).
  */
-function path2Import (path, entryPointPattern, importPattern) {
-  return ('\./' + path).replace(new RegExp(entryPointPattern.replace(/\*/, '\(\.\*\)')), importPattern.replace(/\*/, '\$1'))
+export function resolveImport(entryPoint) {
+  if (typeof entryPoint === 'string') {
+    return entryPoint
+  }
+
+  for (const key of ['browser', 'import', 'default']) {
+    if (entryPoint.hasOwnProperty(key) && entryPoint[key] !== undefined) {
+      return resolveImport(entryPoint[key])
+    }
+  }
+
+  throw new Error('No valid entry point found')
 }
 
-async function bundleExports (packageInfo) {
-  const { name, exports } = packageInfo
-  const entry = import.meta.resolve(name, `file://${projectRoot}/package.json`)
-  const banner = `/* Django-ESM - ${packageInfo.name}@${packageInfo.version} */`
+/**
+ * Resolve an import path from a filesystem path and a subpath pattern.
+ *
+ * See also: https://nodejs.org/api/packages.html#subpath-patterns
+ *
+ * @param {string} filePath - The relative filesystem path.
+ * @param {string} pathPattern - The pattern to match the entry point.
+ * @param {string} entryPointPattern - The pattern to resolve the import path.
+ */
+export function path2EntryPoint(filePath, pathPattern, entryPointPattern) {
+  const pathPatternRexEx = new RegExp(
+    path.normalize(pathPattern).replace(/[.+?^${}()|\[\]\\]/g, '\\$&').replace(
+      /\*/,
+      '\(\.\*\)',
+    ),
+  )
+  if (!pathPatternRexEx.test(filePath)) {
+    throw new Error(`Invalid path ${filePath} for entry point ${pathPatternRexEx}`)
+  }
+  return path.normalize(
+    filePath.replace(pathPatternRexEx, entryPointPattern.replace(/\*/, '\$1')),
+  )
+}
 
-  if (exports === undefined || typeof exports === 'string') {
-    const result = await esbuild.build({
-      ...options,
-      entryPoints: [entry.slice('file://'.length)],
+/**
+ * Expand a subpath pattern to a list of files.
+ *
+ * The *-character represents any string including a / or filesystem separator.
+ *
+ * @param pattern {string}: The subpath pattern to expand.
+ * @param cwd {string}: The current working directory.
+ */
+export async function expandSubpathPattern(pattern, cwd) {
+  return await glob(pattern.replace(/\*/, '{*,**/*}'), {
+    cwd,
+    nodir: true,
+    dotRelative: true,
+    ignore: 'node_modules/**',
+  })
+}
 
-      outbase: projectRoot,
-      banner: {
-        js: banner,
-        css: banner,
-      },
-    })
-    entryPointMap[name] = result.metafile.outputs[0]
+/**
+ * Resolve a package's entry points.
+ *
+ * See also: https://nodejs.org/api/packages.html#package-entry-points
+ *
+ * @param pkgName {string}: The name of the package or # for imports.
+ * @param entryPoints {object|string|string[]}: The entry points (exports/imports).
+ *
+ * @returns {object}: A map of entry points to import paths.
+ */
+export function resolveEntryPoints(pkgName, entryPoints) {
+  if (typeof entryPoints === 'string') {
+    return { [pkgName]: resolveImport(entryPoints) }
+  } else if (Array.isArray(entryPoints)) {
+    return Object.fromEntries(
+      entryPoints.map((
+        entryPoint,
+      ) => [path.join(pkgName, entryPoint), resolveImport(entryPoint)]),
+    )
+  } else if (typeof entryPoints === 'object') {
+    return Object.entries(entryPoints).filter(([key, value]) => value !== undefined)
+      .reduce((acc, [key, value]) => {
+        acc[path.join(pkgName, key)] = resolveImport(value)
+        return acc
+      }, {})
   } else {
-    for (const [exportName, entryPointPattern] of Object.entries(exports)) {
-      const importName = path.join(name, exportName)
-      const result = await esbuild.build({
-        ...options,
-        entryPoints: [import.meta.resolve(importName, `file://${projectRoot}/package.json`).slice('file://'.length)],
-        banner: {
-          js: banner,
-          css: banner,
-        },
-      })
-      for (const [outputFileName, output] of Object.entries(result.metafile.outputs)) {
-        if (output.entryPoint !== undefined) {
-          entryPointMap[importName] = outputFileName
-        }
-      }
-    }
+    throw new Error(`Invalid entry points for package ${pkgName}: ${entryPoints}`)
   }
 }
 
-for (const [importName, entryPointPattern] of Object.entries(imports)) {
+/**
+ * Expand the subpaths for all patterns in the entry points.
+ * @param pkgName {string}: The name of the package or # for imports.
+ * @param entryPoints {object|string|string[]}: The entry points (exports/imports).
+ * @param cwd {string}: The current working directory.
+ * @param projectRoot {string}: The root directory of the project.
+ * @return {Promise<{object}>}: A map of entry points to their file paths.
+ */
+export async function expandEntryPoints(pkgName, entryPoints, cwd, projectRoot) {
+  const entryPointMap = {}
+  for (
+    const [entryPointPattern, pathPattern] of Object.entries(
+      resolveEntryPoints(pkgName, entryPoints),
+    )
+  ) {
+    for (const subpath of await expandSubpathPattern(pathPattern, cwd)) {
+      const importPath = path2EntryPoint(subpath, pathPattern, entryPointPattern)
+      entryPointMap[importPath] = path.relative(projectRoot, path.join(cwd, subpath))
+    }
+  }
+  return entryPointMap
+}
+
+export async function bundleExports(cwd, projectRoot) {
+  const packageInfo =
+    (await import('./' + path.join(cwd, 'package.json'), { with: { type: 'json' } }))[
+      'default'
+    ]
+  return await expandEntryPoints(
+    packageInfo.name,
+    packageInfo.exports || { '.': packageInfo.main },
+    cwd,
+    projectRoot,
+  )
+}
+
+async function main(argv) {
+  const projectRoot = argv[2]
+  const outputDir = argv[3]
+  const rootPackage =
+    (await import(path.join(projectRoot, 'package.json'), { with: { type: 'json' } }))[
+      'default'
+    ]
+
+  let entryPoints = {}
+  entryPoints = {
+    ...entryPoints,
+    ...await expandEntryPoints('', rootPackage.imports, projectRoot, projectRoot),
+  }
+  entryPoints = { ...entryPoints, ...await bundleExports(projectRoot, projectRoot) }
+  for (
+    const dep of Object.keys({
+      ...rootPackage.dependencies,
+      ...rootPackage.peerDependencies,
+    })
+  ) {
+    entryPoints = {
+      ...entryPoints,
+      ...await bundleExports(path.join(projectRoot, 'node_modules', dep), projectRoot),
+    }
+  }
+
+  const reverseEntryPointMap = Object.entries(entryPoints)
+    .reduce((obj, [key, value]) => ({ ...obj, [path.normalize(value)]: key }), {})
+
   const result = await esbuild.build({
-    ...options,
-    entryPoints: [entryPointPattern],
-    banner: {
-      js: banner,
-      css: banner,
-    },
+    entryPoints: Object.values(entryPoints).map((entryPoint) =>
+      path.join(projectRoot, entryPoint)
+    ),
+    bundle: true,
+    format: 'esm',
+    external: Object.keys(entryPoints),
+    outbase: projectRoot,
+    outdir: outputDir,
+    entryNames: '[dir]/[name]-[hash]',
+    minify: true,
+    sourcemap: true,
+    platform: 'browser',
+    target: 'es2020',
+    allowOverwrite: true,
+    metafile: true,
   })
+
+  const entryPointMap = {}
   for (const [name, output] of Object.entries(result.metafile.outputs)) {
     if (output.entryPoint !== undefined) {
-      entryPointMap[path2Import(path.relative(projectRoot, output.entryPoint), entryPointPattern, importName)] = name
+      entryPointMap[
+        reverseEntryPointMap[path.relative(projectRoot, output.entryPoint)]
+      ] = path.relative(outputDir, name)
     }
   }
+
+  const integrity = {}
+  for (const value of Object.values(entryPointMap)) {
+    const filePath = path.join(outputDir, value)
+    const fileContent = await fs.readFile(filePath)
+    integrity[value] = await integrityHash(fileContent)
+  }
+
+  const importMap = {
+    imports: entryPointMap,
+    integrity,
+  }
+
+  await fs.writeFile(path.join(outputDir, 'imports.json'), JSON.stringify(importMap))
 }
 
-for (const dep of Object.keys(dependencies)) {
-  const packageInfo = (await import(`file://${projectRoot}/node_modules/${dep}/package.json`, { with: { type: 'json' } }))['default']
-  await bundleExports(packageInfo)
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  await main(process.argv)
 }
-
-await fs.writeFile(path.join(outputDir, 'imports.json'), JSON.stringify(entryPointMap))
---
+++ tests/esimport.test.js
@@ -0,0 +1,240 @@
+import assert from 'node:assert'
+import { describe, test } from 'node:test'
+
+import * as esimport from 'esimport'
+
+describe('integrityHash', () => {
+  test('SHA-512', async () => {
+    const hash = await esimport.integrityHash('foo')
+    assert.strictEqual(
+      hash,
+      'sha512-9/u6bgY2+JDlb7vzKD5STG+jIErimDgtYkdB0NxmODJuKCxBvl5CVNiCB3LFUYosWowMf37aGVlKfrU5RT4e1w==',
+    )
+  })
+
+  test('SHA-384', async () => {
+    const hash = await esimport.integrityHash('foo', 'SHA-384')
+    assert.strictEqual(
+      hash,
+      'sha384-mMEf/f3VQGdrGhN8saIrKnA1DJpEFx1rEYDGvly7LuP3nVMsih3Z7y6OCOdSo7q7',
+    )
+  })
+
+  test('SHA-256', async () => {
+    const hash = await esimport.integrityHash('foo', 'SHA-256')
+    assert.strictEqual(hash, 'sha256-LCa0a2j/xo/5m0U8HTBBNBNCLXBkg7+g+YpeiGJm564=')
+  })
+
+  test('invalid algorithm', async () => {
+    await assert.rejects(
+      () => esimport.integrityHash('foo', 'invalid'),
+      /Unrecognized algorithm name/,
+    )
+  })
+})
+
+describe('path2Import', () => {
+  test('filename wildcard', () => {
+    assert.strictEqual(esimport.path2EntryPoint('foo.js', './*.js', './*'), 'foo')
+  })
+
+  test('path wildcard', () => {
+    assert.strictEqual(
+      esimport.path2EntryPoint('bar/baz/foo.js', './bar/*.js', './util/*'),
+      'util/baz/foo',
+    )
+  })
+
+  test('no wildcard', () => {
+    assert.strictEqual(esimport.path2EntryPoint('foo.js', './foo.js', './foo'), 'foo')
+  })
+
+  test('no match', () => {
+    assert.throws(
+      () => esimport.path2EntryPoint('foo.js', './bar.js', './foo'),
+      /Invalid path foo.js for entry point/,
+    )
+  })
+})
+
+describe('resolveImport', () => {
+  test('string', () => {
+    assert.strictEqual(esimport.resolveImport('foo'), 'foo')
+  })
+
+  test('import', () => {
+    assert.strictEqual(esimport.resolveImport({ import: 'foo', default: 'bar' }), 'foo')
+  })
+
+  test('default', () => {
+    assert.strictEqual(
+      esimport.resolveImport({
+        default: 'foo',
+        require: 'bar',
+      }),
+      'foo',
+    )
+  })
+
+  test('nested', () => {
+    assert.strictEqual(
+      esimport.resolveImport({
+        import: {
+          types: 'foo.d',
+          default: 'foo',
+        },
+      }),
+      'foo',
+    )
+  })
+
+  test('no entry point', () => {
+    assert.throws(() => esimport.resolveImport({}), /No valid entry point found/)
+  })
+})
+
+describe('resolveEntryPoints', () => {
+  test('string', () => {
+    assert.deepStrictEqual(esimport.resolveEntryPoints('fellowship', './ring.js'), {
+      fellowship: './ring.js',
+    })
+  })
+
+  test('array', () => {
+    assert.deepStrictEqual(
+      esimport.resolveEntryPoints('fellowship', ['./ring.js', './gandalf.js']),
+      {
+        'fellowship/ring.js': './ring.js',
+        'fellowship/gandalf.js': './gandalf.js',
+      },
+    )
+  })
+
+  test('object', () => {
+    assert.deepStrictEqual(
+      esimport.resolveEntryPoints('fellowship', {
+        ring: './ring.js',
+        gandalf: './gandalf.js',
+      }),
+      {
+        'fellowship/ring': './ring.js',
+        'fellowship/gandalf': './gandalf.js',
+      },
+    )
+  })
+
+  test('invalid', () => {
+    assert.throws(
+      () => esimport.resolveEntryPoints('fellowship', 9),
+      /Invalid entry points for package fellowship/,
+    )
+  })
+})
+
+describe('expandSubpathPattern', () => {
+  test('no wildcard', async () => {
+    assert.deepStrictEqual(
+      await esimport.expandSubpathPattern(
+        './src/index.js',
+        'tests/fixtures/fellowship',
+      ),
+      ['./src/index.js'],
+    )
+  })
+
+  test('wildcard w/ subpath', async () => {
+    assert.deepStrictEqual(
+      await esimport.expandSubpathPattern('./src/*.js', 'tests/fixtures/fellowship'),
+      [
+        './src/index.js',
+        './src/hobbits/sam.js',
+        './src/hobbits/frodo.js',
+        './src/dwarfs/gimli.js',
+      ],
+    )
+  })
+
+  test('wildcard w/o subpath', async () => {
+    assert.deepStrictEqual(
+      await esimport.expandSubpathPattern(
+        './src/hobbits/*.js',
+        'tests/fixtures/fellowship',
+      ),
+      [
+        './src/hobbits/sam.js',
+        './src/hobbits/frodo.js',
+      ],
+    )
+  })
+
+  test('no match', async () => {
+    assert.deepStrictEqual(
+      await esimport.expandSubpathPattern('./src/*.js', 'tests/fixtures/rings'),
+      [],
+    )
+  })
+})
+
+describe('expandEntryPoints', () => {
+  test('string', async () => {
+    assert.deepStrictEqual(
+      await esimport.expandEntryPoints(
+        'fellowship',
+        './src/index.js',
+        'tests/fixtures/fellowship',
+        'tests/fixtures/fellowship',
+      ),
+      {
+        fellowship: 'src/index.js',
+      },
+    )
+  })
+
+  test('array', async () => {
+    assert.deepStrictEqual(
+      await esimport.expandEntryPoints(
+        'fellowship',
+        ['./src/index.js', './src/hobbits/*.js'],
+        'tests/fixtures/fellowship',
+        'tests/fixtures/fellowship',
+      ),
+      {
+        'fellowship/src/index.js': 'src/index.js',
+        'fellowship/src/hobbits/sam.js': 'src/hobbits/sam.js',
+        'fellowship/src/hobbits/frodo.js': 'src/hobbits/frodo.js',
+      },
+    )
+  })
+
+  test('object', async () => {
+    assert.deepStrictEqual(
+      await esimport.expandEntryPoints(
+        'fellowship',
+        {
+          '.': './src/index.js',
+          './hobbits/*': './src/hobbits/*.js',
+        },
+        'tests/fixtures/fellowship',
+        'tests/fixtures/fellowship',
+      ),
+      {
+        fellowship: 'src/index.js',
+        'fellowship/hobbits/frodo': 'src/hobbits/frodo.js',
+        'fellowship/hobbits/sam': 'src/hobbits/sam.js',
+      },
+    )
+  })
+})
+
+describe('bundleExports', () => {
+  test('exports', async () => {
+    assert.deepStrictEqual(
+      await esimport.bundleExports('tests/fixtures/fellowship', 'tests/fixtures/'),
+      {
+        fellowship: 'fellowship/src/index.js',
+        'fellowship/hobbits/frodo.js': 'fellowship/src/hobbits/frodo.js',
+        'fellowship/hobbits/sam.js': 'fellowship/src/hobbits/sam.js',
+      },
+    )
+  })
+})
