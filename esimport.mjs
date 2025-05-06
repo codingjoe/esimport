2e2026110ab98bdf3e91f7afe0f02726c656f769
Johannes Maron <johannes@maron.family>
2025-05-06 16:45:46 +0200

    Update build target to ES2022

--- esimport.mjs
+++ esimport.mjs
@@ -457,7 +457,7 @@ export async function run(packageDir, outputDir, options) {
     minify: true,
     sourcemap: true,
     platform: 'browser',
-    target: 'es2020',
+    target: 'es2022',
     allowOverwrite: true,
     metafile: true,
     plugins: [new UnenvResolvePlugin()],
